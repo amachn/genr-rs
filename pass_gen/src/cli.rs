d5188ec92353a6e54cfd5514ad3d70dbc3fa8b78
amachn <amachann2005@gmail.com>
2023-03-09 13:37:01 +0000

    chore: fix commenting

--- pass_gen/src/cli.rs
+++ pass_gen/src/cli.rs
@@ -7,19 +7,19 @@ pub struct Cli {
     #[arg(short, long, default_value_t = 8)]
     pub length: u8,
 
-    /// Disable uppercase letters
+    /// Disable uppercase letters.
     #[arg(long, visible_alias = "nu", default_value_t = false, action = ArgAction::SetTrue)]
     pub no_upper: bool,
 
-    /// Disable lowercase letters
+    /// Disable lowercase letters.
     #[arg(long, visible_alias = "nl", default_value_t = false, action = ArgAction::SetTrue)]
     pub no_lower: bool,
 
-    /// Disable numbers
+    /// Disable numbers.
     #[arg(long, visible_alias = "nn", default_value_t = false, action = ArgAction::SetTrue)]
     pub no_num: bool,
 
-    /// Enable symbols
+    /// Enable symbols.
     #[arg(short, long, default_value_t = false, action = ArgAction::SetTrue)]
     pub symbols: bool,
 }
