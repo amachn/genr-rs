4a414ab850a7846edc52f50b985fe120f68748f6
amachn <amachann2005@gmail.com>
2023-03-11 01:53:49 +0000

    feat(generator): combine CLI and generator

--- pass_gen/src/generator.rs
+++ pass_gen/src/generator.rs
@@ -1,14 +1,34 @@
-use std::fmt;
+use clap::{ArgAction, Parser};
 
-use crate::cli::Cli;
+use std::{fmt, process};
 
-#[derive(Debug)]
+/// A basic password generator written in Rust.
+#[derive(Debug, Parser)]
 pub struct Generator {
-    pub len: u8,
+    /// The length of the password to be generated.
+    #[arg(short, long, visible_alias = "len", default_value_t = 8)]
+    pub length: u8,
+
+    // the logic for upper/lower/num/sym is a bit goofy, but makes CLI usage simpler for the user
+
+    /// Disable uppercase letters.
+    #[arg(long = "no_upper", visible_alias = "nu", default_value_t = true, action = ArgAction::SetFalse)]
     pub upper: bool,
+
+    /// Disable lowercase letters.
+    #[arg(long = "no_lower", visible_alias = "nl", default_value_t = true, action = ArgAction::SetFalse)]
     pub lower: bool,
+
+    /// Disable numbers.
+    #[arg(long = "no_num", visible_alias = "nn", default_value_t = true, action = ArgAction::SetFalse)]
     pub num: bool,
+
+    /// Enable symbols.
+    #[arg(short, long, visible_alias = "symbols", default_value_t = false, action = ArgAction::SetTrue)]
     pub sym: bool,
+
+    // this one is not an argument in the Parser and is exclusively for generation use
+    #[clap(skip)]
     pub generated: String,
 }
 
@@ -19,27 +39,31 @@ impl fmt::Display for Generator {
 }
 
 impl Generator {
-    pub fn new(args: Cli) -> Generator {
-        // destructure the parsed arguments into variables
-        let Cli { 
-            length: len, 
-            no_upper: mut upper, 
-            no_lower: mut lower, 
-            no_num: mut num, 
-            symbols: sym 
-        } = args; 
-        let generated = String::from("");
-
-        // flip booleans to match
-        upper = !upper;
-        lower = !lower;
-        num = !num;
-
-        // initialize and return Generator struct
-        Generator { len, upper, lower, num, sym, generated }
+    pub fn new() -> Generator {
+        let mut generator = Self::parse(); 
+        match Self::validate(&generator) {
+            Ok(_) => { 
+                generator.generated = String::new();
+                generator 
+            },
+            Err(error) => {
+                println!("{}", error);
+                process::exit(1)
+            },
+        }
     }
 
     pub fn make(&mut self) -> () {
         
+    }
+
+    pub fn validate(&self) -> Result<(), &'static str> {
+        if self.upper && self.lower && self.num && self.sym && false { // if all charsets are off
+            Err("ERROR: Cannot disable all possible characters!")
+        } else if self.length < 4 || self.length > 64 { // if length is <4 or >64
+            Err("ERROR: Possible lengths range from 4-64 only!")
+        } else { // otherwise, arguments are valid
+            Ok(())
+        }
     }
 }
